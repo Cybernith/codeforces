d238bf1b7efc3c505a3b64e46ba7764941192b11
Cybernith <93315815+Cybernith@users.noreply.github.com>
2025-11-16 00:54:03 +0330

    Update awnser.py

--- SpyDetected/awnser.py
+++ SpyDetected/awnser.py
@@ -1,34 +1,36 @@
-input = [
-    5,
-    4,
-    [11, 11, 13, 11],
-    5,
-    [4, 4, 7, 4, 4],
-    10,
-    [3, 3, 3, 3, 10, 3, 3, 3, 3, 3],
-    4,
-    [11, 11, 11, 18],
-    3,
-    [20, 20, 10],
-]   
+import sys
 
-for i in range(1, input[0]+1):
-    dic = {}
-    numbers = []
-    for x in input[i*2]:
-        if not x in numbers:
-            numbers.append(x)
-            dic[x] = 1
-        else: 
-            dic[x] += 1
-            
-    for num in dic:
-        if dic[num] == 1:
-            x = 1
-            for value in input[i*2]:
-                if value == num:
-                    print(x)
-                x += 1
-        
-    
-    
+
+def main() -> None:
+    data = sys.stdin.read().strip().split()
+    if not data:
+        return
+
+    t = int(data[0])
+    idx = 1
+
+    out_lines = []
+
+    for _ in range(t):
+        n = int(data[idx])
+        idx += 1
+        arr = list(map(int, data[idx:idx + n]))
+        idx += n
+
+        if arr[0] == arr[1]:
+            common = arr[0]
+            for i in range(n):
+                if arr[i] != common:
+                    out_lines.append(str(i + 1)) 
+                    break
+        else:
+            if arr[0] == arr[2]:
+                out_lines.append("2")
+            else:
+                out_lines.append("1")
+
+    sys.stdout.write("\n".join(out_lines))
+
+
+if __name__ == "__main__":
+    main()
